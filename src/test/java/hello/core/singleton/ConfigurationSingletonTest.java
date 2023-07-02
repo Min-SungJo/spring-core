a447c59f5c199034242cd6310a97a36d907814de
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-07-02 13:46:42 +0900

    @Configuration 과 바이트코드 조작의 마법 (SpringCGLIB, @Configuration)

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -25,4 +25,13 @@ public class ConfigurationSingletonTest {
         System.out.println("orderService -> memberRepository = " + memberRepository2);
         System.out.println("memberRepository -> memberRepository = " + memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+        // SpringCGLIB 덕분에 싱글톤 보장이 됨(이미 스프링 빈이 존재하면 기존 스프링 빈을 반환, 없으면 새롭게 생성)
+        // SpringCGLIB 을 활용하지 않으면, 스프링 컨테이너에서 관리하는 클래스가 호출되지 않을 수도 있음
+    }
 }
