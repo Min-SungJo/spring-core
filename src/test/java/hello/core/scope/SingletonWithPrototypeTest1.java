1ec88be3fdfc6bfb930636e72c6517b0829591db
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-07-03 15:37:51 +0900

    Prototype Scope SingletonBean 과 함께 사용으로 발생하는 문제, Provider 로 해결 (Provider 를 통해 순환참조 문제 또한 해결할 수 있음)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,9 +2,12 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -36,21 +39,33 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2); // 이미 주입된 Bean
-//        assertThat(count2).isEqualTo(1);
+//        assertThat(count2).isEqualTo(2); // 이미 주입된 Bean
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
 //    @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
 
+        //ObjectProvider
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider; // ObjectFactory 상속, 추가적으로 기능을 제공
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        // 생성자로 해도 되지만 필드주입이 간단하니, 이 예시에서는 필드주입 사용
+
+//        private final PrototypeBean prototypeBean; // 생성시점에 주입
+//        @Autowired
+//        ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+
+        //JSR-330 Provider, 자바 표준(ObjectProvider 는 스프링에 의존적임), 단순함, 라이브러리 추가해야함(ObjectProvider 는 추가 필요 x)
         @Autowired
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
@@ -66,24 +81,24 @@ public class SingletonWithPrototypeTest1 {
 //            return count;
 //        }
     }
-    @Scope("singleton")
-//    @RequiredArgsConstructor
-    static class ClientBean2 {
-        private final PrototypeBean prototypeBean;
         // 여러 빈에서 같은 PrototypeBean 을 주입 받으면, 각 시점마다 새로운 PrototypeBean 이 생성됨 (x01, x02)
         // 사용할 때마다 생성되는 것은 아님
-
-        @Autowired
-        ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic() {
-            prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
-        }
-    }
+//    @Scope("singleton")
+////    @RequiredArgsConstructor
+//    static class ClientBean2 {
+//        private final PrototypeBean prototypeBean;
+//
+//        @Autowired
+//        ClientBean2(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+//
+//        public int logic() {
+//            prototypeBean.addCount();
+//            int count = prototypeBean.getCount();
+//            return count;
+//        }
+//    }
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
